15fac3bf80dde10a775769656b4a0f2ab5bb80e6
Chris Borkert <digplan@outlook.com>
2025-06-02 22:47:22 -0500

    api

--- base-classes/API.js
+++ base-classes/API.js
@@ -1,27 +1,24 @@
 import APIDEF from './APIDEF.js';
 
-globalThis.API ??= class API {
-  constructor() {
-    // create api methods
-    Object.entries(APIDEF).forEach(([key]) => this[key] = (vars, proxy) => this.exec(key, vars, proxy));
-  }
+const API = Object.create(null);
+Object.entries(APIDEF).forEach(([key]) => this[key] = (vars, proxy) => API.exec(key, vars, proxy));
 
-  static async fetch(options, proxy) {
+API.fetch = async (options, proxy) => {
     const r = await fetch(proxy || options.url, proxy ? { method: 'POST', body: JSON.stringify(options) } : options);
     return { response: r, request: options, proxy, json: await r.json() };
-  }
+}
 
-  static exec(name, vars = {}, proxy) {
+API.exec = (name, vars = {}, proxy) => {
     if (!APIDEF[name]) throw new Error(`Unknown command: ${name}`);
     if (typeof vars === 'string') return { def: APIDEF[name], params: this.getParams(name) };
     return this.fetch(this.parse(APIDEF[name], vars), proxy);
-  }
+ }
 
-  static getDef(name) {
+API.getDef = (name) => {
     return name ? { command: APIDEF[name], params: APIDEF[name].match(/\$(\w+)/g) ?? [] } : APIDEF;
-  }
+}
 
-  static parse(command, vars) {
+API.parse = (command, vars) => {
     try {
       command = command.replace(/\$(\w+)/g, (_, v) => v in vars ? (typeof vars[v] === 'object' ? JSON.stringify(vars[v]) : vars[v]) : `$${v}`);
       const [method, url, headers, body] = command.split('\n');
@@ -35,7 +32,7 @@ globalThis.API ??= class API {
       console.error('Parse error:', command, vars);
       throw e;
     }
-  }
 }
 
-export default globalThis.API;
+globalThis.API = API;
+export default API;
